a3bb02b97e5c267173ea2038354574a2657228ea
Fabian Peternek <fabian.peternek@tu-dortmund.de>
2011-11-21 17:21:09 -0500

    adaboost modification

--- code/run_submission.m
+++ code/run_submission.m
@@ -40,7 +40,8 @@ X = make_sparse(train);
 Y = double([train.rating]');
 
 Xtest = make_sparse(test, size(X, 2));
-addpath (genpath('liblinear'));
+%%
+addpath(genpath('liblinear'));
 possibleTs = 2:7;
 rmse = zeros(1,numel(possibleTs));
 err = zeros(1,numel(possibleTs));
@@ -51,13 +52,44 @@ for T = possibleTs
     [rmse(i), err(i)] = xval_error(train, X, Y, tr_hand, te_hand);
     i = i+1;
 end
+%%
 plot(possibleTs, rmse, possibleTs, err);
-%% Adaboost with T = 40 xval
-tr_hand = @(X,Y) adaboost(X,Y,10);
+%% Adaboost xval for singular value
+tr_hand = @(X,Y) adaboost(X,Y,6);
 te_hand = @(c,x) round(adaboost_test(c,x));
-[rmse(i), err(i)] = xval_error(train, X, Y, tr_hand, te_hand);
+[rmse_s, err_s] = xval_error(train, Z, Y, tr_hand, te_hand);
 
 %% Liblinear xval
 tr_hand = @(X,Y) liblinear_train(Y,X, '-s 6 -e 1.0');
 te_hand = @(c,x) liblinear_predict(ones(size(x,1),1), x, c);
 [rmse, err] = xval_error(train, X, Y, tr_hand, te_hand);
+
+%% k-nn xval with random projection to two dimensions
+[Z, Zt] = random_projection(X,Xtest,2);
+Z = full(Z);
+Zt = full(Zt);
+% Unfortunately my xval-function doesn't work with knn.
+part = [train.category];
+N = max(part);
+e = zeros(1,N);
+rm = zeros(1,N);
+
+t = CTimeleft(N);
+for i = 1:N
+    t.timeleft();
+    % Compute training set
+    Di = Z(part ~= i, :);
+    % Training labels
+    Yi = Y(part ~= i);
+    % Test fold and expected answers
+    TX = Z(part == i, :);
+    TY = Y(part == i);
+    % Train classifier with training set
+    %classifier = train_handle(Di, Yi);
+    % Compute error on i'th fold
+    Yhat_i = knn_test(3, Di, Yi, TX); % trying 3-nearest neighbors
+    e(i) = 1/size(TX,1) * (sum(Yhat_i ~= TY));
+    rm(i) = sqrt(1/size(TX,1) * sum((TY - Yhat_i).^2));
+end
+error = 1/double(N)*sum(e);
+rmse = 1/double(N)*sum(rm);
